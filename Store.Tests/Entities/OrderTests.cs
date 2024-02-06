41d00be30cb0b09a2e68d495189eff1ca88bb8aa
Leonardo Romeiro <leozinwork@gmail.com>
2024-02-06 13:21:13 -0300

    Iniciando teste

---
+++ Store.Tests/Entities/OrderTests.cs
@@ -0,0 +1,22 @@
+﻿using Store.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Tests.Entities
+{
+
+    [TestClass]
+    public class OrderTests
+    {
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void DadoUmNovoPedidoValidoEleDeveGerarUmNumeroCom8Caracteres()
+        {
+            var customer = new Customer("Leonardo", "Teste@gmail.com");
+            var order = new Or
+        }
+    }
+}
