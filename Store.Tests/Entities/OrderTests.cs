7ac64422651974f6fe8e110b1b7b1b48a427c1ee
Leonardo Romeiro <leozinwork@gmail.com>
2024-02-08 18:01:15 -0300

    Criando teste :DadoUmNovoPedidoSeuStatusDeveSerAguardandoPagamento

--- Store.Tests/Entities/OrderTests.cs
+++ Store.Tests/Entities/OrderTests.cs
@@ -1,4 +1,5 @@
 ﻿using Store.Domain.Entities;
+using Store.Domain.Enums;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,12 +12,26 @@ namespace Store.Tests.Entities
     [TestClass]
     public class OrderTests
     {
+        private readonly Customer _customer = new Customer("André Baltieri", "andre@balta.io");
+        private readonly Product _product = new Product("Produto 1", 10, true);
+        private readonly Discount _discount = new Discount(10, DateTime.Now.AddDays(5));
+
         [TestMethod]
         [TestCategory("Domain")]
         public void DadoUmNovoPedidoValidoEleDeveGerarUmNumeroCom8Caracteres()
         {
             var customer = new Customer("Leonardo", "Teste@gmail.com");
-            var order = new Or
+            var order = new Order(customer, 0, null);
+            Assert.AreEqual(8, order.Number.Length);
         }
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void DadoUmNovoPedidoSeuStatusDeveSerAguardandoPagamento()
+        {
+            var order = new Order(_customer, 0, null);
+            Assert.AreEqual(order.Status, EOrderStatus.WaitingPayment);
+        }
+
     }
 }
